f88489d457e5b127c6113f0d8056267bf6c00279
Shubham Mishra <shubham.mishra@salesforce.com>
2022-01-19 01:50:10 +0530

    Added navigation bar and first logo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Navigation from './componenets/Navigation';
 
 function App() {
   return (
     <div className="App">
+      <Navigation/>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Work in progress for <code>News Bulletin</code>.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <hr/><hr/>
+        <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
   );
---
+++ src/componenets/Navigation.jsx
@@ -0,0 +1,16 @@
+import { Navbar, Container, Nav } from "react-bootstrap";
+
+export default function Navigation() {
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#bulletin">News Express</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#bulletin">Bulletin</Nav.Link>
+          <Nav.Link href="#addNews">Add News</Nav.Link>
+          <Nav.Link href="#filterNews">Filter News</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+}
